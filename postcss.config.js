9006d6d425711e98423547115090d04885a1ee9d
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-07-10 18:48:02 +0300

    apply tailwind classes to .blink class

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
+let prefixOverrideList = ['html', 'body'];
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-prefix-selector': {
+      prefix: '.blink',
+      includeFiles: ['index.css'],
+      transform: function (prefix, selector, prefixedSelector) {
+        if (selector.startsWith('.blink')) {
+          return selector;
+        }
+        if (prefixOverrideList.includes(selector)) {
+          return prefix;
+        } else {
+          return prefixedSelector;
+        }
+      },
+    },
   },
-}
+};
