f97aeebb6d91fa2f5f7d4a76d8ab041fed9d1ff6
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-07-10 15:11:32 +0300

    add set proxy url

--- src/utils/proxify.ts
+++ src/utils/proxify.ts
@@ -1,8 +1,30 @@
-const proxyUrl = 'https://proxy.dial.to';
+let proxyUrl: string | null = 'https://proxy.dial.to';
+
+export function setProxyUrl(url: string): void {
+  if (!url) {
+    console.warn(
+      '[@dialectlabs/blinks] Proxy URL is not set, proxy will be disabled',
+    );
+    proxyUrl = null;
+    return;
+  }
+
+  try {
+    new URL(url);
+  } catch (e) {
+    console.warn('[@dialectlabs/blinks] Invalid proxy URL', e);
+    return;
+  }
+
+  proxyUrl = url;
+}
 
 export function proxify(url: string): URL {
   const baseUrl = new URL(url);
   if (baseUrl.hostname === 'localhost' || baseUrl.hostname === '127.0.0.1') {
+    return baseUrl;
+  }
+  if (!proxyUrl) {
     return baseUrl;
   }
   const proxifiedUrl = new URL(proxyUrl);
