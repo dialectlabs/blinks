eda8dd0c2db58d4249131bb147592416fddad70a
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2024-08-10 11:12:40 +0300

    add ethereum mainnet to caip-2 constants

--- src/utils/caip-2.ts
+++ src/utils/caip-2.ts
@@ -5,12 +5,12 @@ export const BlockchainIds = {
   SOLANA_MAINNET: 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp',
   SOLANA_DEVNET: 'solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1',
   SOLANA_TESTNET: 'solana:4uhcVJyU9pJkvQyS88uRDiswHXSCkY3',
+  ETHEREUM_MAINNET: 'eip155:1',
 };
 
 export const BlockchainNames: Record<string, string> = {
   [BlockchainIds.SOLANA_MAINNET]: 'Solana Mainnet',
   [BlockchainIds.SOLANA_DEVNET]: 'Solana Devnet',
   [BlockchainIds.SOLANA_TESTNET]: 'Solana Testnet',
+  [BlockchainIds.ETHEREUM_MAINNET]: 'Ethereum Mainnet',
 };
-
-// TODO: add ethereum, and a few more blockchains
