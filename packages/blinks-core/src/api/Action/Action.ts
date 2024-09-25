9d9986b9ef5a3181e0c10357055de7540e363521
Victoria Prusakova <prsvic@gmail.com>
2024-09-25 12:04:36 +0300

    replace nanoid with nanoid/non-secure

--- packages/blinks-core/src/api/Action/Action.ts
+++ packages/blinks-core/src/api/Action/Action.ts
@@ -1,4 +1,4 @@
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid/non-secure';
 import { proxify, proxifyImage } from '../../utils';
 import { isUrlSameOrigin } from '../../utils/security.ts';
 import type { ActionAdapter } from '../ActionConfig.ts';
