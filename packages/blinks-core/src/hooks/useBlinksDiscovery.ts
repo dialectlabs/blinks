b5091256b3daeef933cf7db4c28c6b287b339ad1
Filipp Sher <filipp.sher@gmail.com>
2024-10-28 18:02:24 +0200

    chore: add interfaces

--- packages/blinks-core/src/hooks/useBlinksDiscovery.ts
+++ packages/blinks-core/src/hooks/useBlinksDiscovery.ts
@@ -4,6 +4,16 @@ interface UseBlinksDiscoveryArgs {
   blinkUrl?: string; // filter by blinkUrl
 }
 
+export interface BlinkDiscoveryEntry {
+  id: string;
+  blinkUrl: string;
+  metadataUrl?: string;
+  title: string;
+  description: string;
+  icon?: string;
+  logo: string;
+}
+
 export const useBlinksDiscovery = (props: UseBlinksDiscoveryArgs) => {
   const [loading, setLoading] = useState(false);
   const refetch = () => {
@@ -26,6 +36,6 @@ export const useBlinksDiscovery = (props: UseBlinksDiscoveryArgs) => {
         icon: 'https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/',
         logo: 'https://dial.to/icon.svg?1741476bec0bda5d',
       },
-    ],
+    ] as BlinkDiscoveryEntry[],
   };
 };
--- packages/blinks-core/src/hooks/useMetadata.ts
+++ packages/blinks-core/src/hooks/useMetadata.ts
@@ -5,6 +5,14 @@ interface UseMetadataArgs {
   url: string; // metadata url
 }
 
+export interface MetadataDataRow {
+  key: string;
+  title: string;
+  value: string;
+  icon?: string;
+  url?: string;
+}
+
 export const useMetadata = (props: UseMetadataArgs) => {
   const [loading, setLoading] = useState(false);
   const refetch = () => {
@@ -20,6 +28,6 @@ export const useMetadata = (props: UseMetadataArgs) => {
     data: [
       { key: 'apy', title: 'Apy', value: '7%' },
       { key: 'description', title: 'Description', value: 'Markdown *text*' },
-    ],
+    ] as MetadataDataRow[],
   };
 };
