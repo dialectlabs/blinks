f917d14370404020eccc0ef61eae8c671c3a784e
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-11-27 21:35:44 +0200

    fix format

--- packages/blinks/src/utils/base64.ts
+++ packages/blinks/src/utils/base64.ts
@@ -1,22 +1,22 @@
 // This approach is written in MDN.
 // btoa does not support utf-8 characters. So we need a little bit hack.
 export const encodeBase64 = (buf: ArrayBufferLike): string => {
-  let binary = ''
-  const bytes = new Uint8Array(buf)
+  let binary = '';
+  const bytes = new Uint8Array(buf);
   for (let i = 0, len = bytes.length; i < len; i++) {
-    binary += String.fromCharCode(bytes[i])
+    binary += String.fromCharCode(bytes[i]);
   }
-  return btoa(binary)
-}
+  return btoa(binary);
+};
 
 // atob does not support utf-8 characters. So we need a little bit hack.
 export const decodeBase64 = (str: string): Uint8Array => {
-  const binary = atob(str)
-  const bytes = new Uint8Array(new ArrayBuffer(binary.length))
-  const half = binary.length / 2
+  const binary = atob(str);
+  const bytes = new Uint8Array(new ArrayBuffer(binary.length));
+  const half = binary.length / 2;
   for (let i = 0, j = binary.length - 1; i <= half; i++, j--) {
-    bytes[i] = binary.charCodeAt(i)
-    bytes[j] = binary.charCodeAt(j)
+    bytes[i] = binary.charCodeAt(i);
+    bytes[j] = binary.charCodeAt(j);
   }
-  return bytes
-}
+  return bytes;
+};
